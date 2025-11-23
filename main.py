0b7570262ce43fff416f9f74d8c929945c972869
Heath Hile <95439485+heathhile@users.noreply.github.com>
2025-11-23 16:04:42 -0500

    added logger stuff

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 import pygame
+from logger import log_state
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while True:
-        screen.fill("black")
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
-        pygame.display.flip()
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        dt = clock.tick(60) / 1000
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        player.update(dt)
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+
+    pygame.quit()
+
 
 if __name__ == "__main__":
     main()
