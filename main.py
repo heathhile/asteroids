31554a4d3eded4e0847bdf75f43856f139debd85
Heath Hile <95439485+heathhile@users.noreply.github.com>
2025-11-23 21:31:05 -0500

    ship moves, asteroids spawn

--- main.py
+++ main.py
@@ -2,34 +2,42 @@ import pygame
 from logger import log_state
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    asteroids = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+
+    asteroid_field = AsteroidField()
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        dt = clock.tick(60) / 1000
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
 
-        player.update(dt)
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for things in drawable:
+            things.draw(screen)
         pygame.display.flip()
-
-    pygame.quit()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
